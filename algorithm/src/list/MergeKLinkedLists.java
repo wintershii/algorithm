603614c5c112a38daedfeaf7b51f4cc0d9361174
wintershii <wintershii@163.com>
2018-12-12 17:32:10 +0800

    leetcode

---
+++ algorithm/src/list/MergeKLinkedLists.java
@@ -0,0 +1,47 @@
+package list;
+
+import java.util.Arrays;
+
+/**
+ * 题目：合并ｋ个排序链表
+ * 来源:leetcode字节跳动专栏
+ */
+public class MergeKLinkedLists {
+    public static void main(String[] args) {
+        ListNode l1 = MergeLinkedList.createList();
+        ListNode l2 = MergeLinkedList.createList();
+        ListNode l3 = MergeLinkedList.createList();
+        MergeLinkedList.printList(l1);
+        MergeLinkedList.printList(l2);
+        MergeLinkedList.printList(l3);
+
+        ListNode[] lists = new ListNode[3];
+        lists[0] = l1;
+        lists[1] = l2;
+        lists[2] = l3;
+
+        MergeLinkedList.printList(new MergeKLinkedLists().mergeKLists(lists));
+
+    }
+
+    /**
+     * 自己写的,基于两个链表合并,改写
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode result = null;
+
+        for (int i = 0; i < lists.length; i++) {
+            if (i == 0) {
+                result = MergeLinkedList.mergeTwoLists(lists[0],lists[1]);
+            }
+            if (i >= 2) {
+                result = MergeLinkedList.mergeTwoLists(result,lists[i]);
+            }
+        }
+        return result;
+    }
+
+
+}
--- algorithm/src/string/SimplifiedPath.java
+++ algorithm/src/string/SimplifiedPath.java
@@ -1,9 +1,6 @@
 package string;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * 题目：简化路径
@@ -11,27 +8,33 @@ import java.util.List;
  */
 public class SimplifiedPath {
     public static void main(String[] args) {
-        System.out.println(new SimplifiedPath().simplifyPath("/a/./b/../../c/"));
+        System.out.println(new SimplifiedPath().simplifyPath("/a/./b/../c/"));
     }
 
     /**
      * 先将字符串根据"/"分开
      * 利用栈，当出现"."时忽略,当出现".."时弹栈,当出现""时不管
      * 最后用String.join这个方法，使每个字符串之间加入了"/"
+     *
+     *
+     * 注意！！！　本题不能使用Deque来模拟栈，因为在最后的 List list = new ArrayList(stack)时
+     * 构造器接受的参数类型　如果用Deque，就会被当成队列，然后顺序就会反
      * @param path
      * @return
      */
     public String simplifyPath(String path) {
-        Deque<String> stack = new ArrayDeque<>();
+        Stack<String> stack = new Stack<>();
         String[] strings = path.split("/");
         for (String str : strings) {
             if ( !stack.isEmpty() && str.equals("..")) {
                 stack.pop();
-            } else if (!str.equals(".") && !str.equals("") && !str.equals("..")) {
+            } else if ( !str.equals(".") && !str.equals("") && !str.equals("..")) {
                 stack.push(str);
             }
         }
+
         List<String> list = new ArrayList<>(stack);
         return "/" + String.join("/",list);
+
     }
 }
