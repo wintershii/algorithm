4376351925ec118067df441544b99a0d9401526b
wintershii <wintershii@163.com>
2018-12-24 21:08:34 +0800

    leetcode

--- algorithm/src/list/SortList.java
+++ algorithm/src/list/SortList.java
@@ -9,7 +9,36 @@ public class SortList {
 
     }
 
+    /**
+     * 对单链表使用归并排序
+     * @param head
+     * @return
+     */
     public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        return merge(sortList(head),sortList(slow));
+    }
 
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1,l2.next);
+            return l2;
+        }
     }
 }
