3a14c40a6127b4e8e5eb7295ed9ec8737a8038ce
wintershii <wintershii@163.com>
2019-04-28 22:24:45 +0800

    leetcode

--- algorithm/src/dp/BugAndSellStock.java
+++ algorithm/src/dp/BugAndSellStock.java
@@ -16,6 +16,7 @@ public class BugAndSellStock {
      */
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
+        int minChange = Integer.MAX_VALUE;
         int maxProfit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minPrice) {
---
+++ algorithm/src/leetcode/CombinationSum_39.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.*;
+
+public class CombinationSum_39 {
+    public static void main(String[] args) {
+        System.out.println(new CombinationSum_39().combinationSum(new int[]{1,2},4));
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        search(candidates,target,0,new ArrayDeque<>(),list);
+        return list;
+    }
+
+    public static void search(int[] candidates, int target, int now, Deque<Integer> nums, List<List<Integer>> list) {
+        if (now > target) {
+            return;
+        }
+        if (now == target) {
+            List<Integer> tmp = new ArrayList<>(nums);
+            for (List<Integer> check : list) {
+                if (check(tmp,check))
+                    return;
+            }
+
+            list.add(tmp);
+        }
+        for (int i = 0; i < candidates.length; i++) {
+            if (now + candidates[i] > target) continue;
+            nums.push(candidates[i]);
+            search(candidates,target,now+candidates[i],nums,list);
+            nums.pop();
+        }
+    }
+
+    public static boolean check(List<Integer> l1, List<Integer> l2) {
+        if (l1.size() != l2.size()) {
+            return false;
+        }
+        Collections.sort(l1);
+        Collections.sort(l2);
+        for (int i = 0; i < l1.size(); i++) {
+            if (l1.get(i).intValue() != l2.get(i).intValue()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
