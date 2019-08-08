f0d65b2c0c05be8fc2513ae3bec7f06b4af7fa15
wintershii <wintershii@163.com>
2019-08-08 21:47:51 +0800

    leetcode

---
+++ leetcode/src/leetcode/HammingWeight_191.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class HammingWeight_191 {
+    public int hammingWeight(int n) {
+        int res = 0;
+        int len = Integer.toBinaryString(n).length();
+        while (len-- > 0) {
+            res += n & 1;
+            n >>= 1;
+        }
+        return res;
+    }
+}
