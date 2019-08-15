ee0d79a954fed9daecb4be6ea98a6639e0d10fd8
wintershii <wintershii@163.com>
2019-08-15 21:17:24 +0800

    dp

---
+++ leetcode/src/dp/_4Triangle.java
@@ -0,0 +1,38 @@
+package dp;
+
+/**
+ * 走三角形
+ * 二维数组, 保存了一个三角形, 求从三角形顶端到底端各数字和最小的路径之和
+ * 每次可以向下走相邻两个位置
+ */
+public class _4Triangle {
+
+
+    public static void main(String[] args) {
+        System.out.println(new _4Triangle().minimumTotal(new int[][]{
+                {2},
+                {3,4},
+                {6,5,7},
+                {4,1,8,3}
+        }));
+    }
+
+    /**
+     * 状态转移方程 : dp[i][j] = num[i][j] + MIN(dp[i+1][j],dp[i+1][j+1])
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal(int[][] triangle) {
+        int m = triangle.length;
+        int[][] dp = new int[m][m];
+        for (int i = 0; i < triangle[m-1].length; ++i) {
+            dp[m-1][i] = triangle[m-1][i];
+        }
+        for (int i = m - 2; i >= 0; --i) {
+            for (int j = 0;j < triangle[i].length; ++j) {
+                dp[i][j] = triangle[i][j] + Math.min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
---
+++ leetcode/src/dp/_6LengthOfLIS.java
@@ -0,0 +1,41 @@
+package dp;
+
+/**
+ * 最长上升子序列
+ */
+public class _6LengthOfLIS {
+
+    public static void main(String[] args) {
+        System.out.println(new _6LengthOfLIS().lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+    }
+
+
+    /**
+     * 状态转移方程 , 双层循环, 内部循环从0-i
+     * if(nums[i] > nums[j] && dp[i] <= dp[j])
+     * dp[i] = dp[j] + 1
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length < 1 ? 0 : 1;
+        }
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        int res = Integer.MIN_VALUE;
+        for (int i = 2; i <= nums.length; ++i) {
+            dp[i] = 1;
+            for (int j = 1; j < i; ++j) {
+                if (nums[i-1] > nums[j-1] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            if (dp[i] > res) {
+                res = dp[i];
+            }
+        }
+        return res;
+    }
+}
