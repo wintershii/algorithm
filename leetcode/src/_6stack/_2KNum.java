97408f3edfaa8f2ae7c7d9c8d8e41eaa87b6ea33
wintershii <wintershii@163.com>
2019-09-22 09:46:55 +0800

    leetcode

--- leetcode/src/_6stack/_2KNum.java
+++ leetcode/src/_6stack/_2KNum.java
@@ -32,4 +32,61 @@ public class _2KNum {
         }
         return heap.peek();
     }
+
+
+    public int findKthLargest2(int[] nums, int k) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+
+        int target = len - k;
+
+        while (true) {
+            int index = partition(nums,left,right);
+            if (index == target) {
+                return nums[target];
+            } else if (index < target) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+        }
+    }
+
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] >= pivot)
+                right--;
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot)
+                left++;
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
+
+//        int pivot = nums[left];
+//        int j = left;
+//        for (int i = left + 1; i <= right; i++) {
+//            if (nums[i] < pivot) {
+//                // 小于 pivot 的元素都被交换到前面
+//                j++;
+//                swap(nums, j, i);
+//            }
+//        }
+//        // 最后这一步不要忘记了
+//        swap(nums, j, left);
+//        return j;
+
+    }
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
 }
